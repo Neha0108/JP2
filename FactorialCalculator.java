af2362a3b44abebe4a50a1084bb6268b26e50b4e
Neha0108 <112578569+Neha0108@users.noreply.github.com>
2024-08-22 11:08:10 +0530

    Update FactorialCalculator.java

--- FactorialCalculator.java
+++ FactorialCalculator.java
@@ -1,15 +1,8 @@
-import java.util.Scanner;
-
 public class FactorialCalculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        int number = 5;  // Predefined number
 
         System.out.println("Factorial of " + number + " is " + factorial(number));
-
-        scanner.close();
     }
 
     public static int factorial(int n) {
